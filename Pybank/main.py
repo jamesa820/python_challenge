9d9aa564c2a6119091f911bca8d7c1355d4ba98a
Jamesa820 <jamesa820@gmail.com>
2020-02-26 20:03:00 -0600

    found total months and profit/loss total

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,7 +1,46 @@
-import csv
+import os
+import csv 
 
-with open('budget_data.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+#reading file
+with open('budget_data.csv') as budget:
+    csv_reader = csv.reader(budget)
+    
 
-    for line in csv_reader:
-        print(line)
+
+    csv_header= next(csv_reader)
+
+    total_months = 0
+    PL_total = 0
+
+    # find total number of months
+    for row in csv_reader:
+        total_months = total_months+1
+    
+    # find total sum of profit and losses   
+        PL_total = PL_total + int(row[1])
+        
+    
+
+
+
+    print('Total Months:' + str(total_months))
+    print('Total: $' + str(PL_total))
+    # print(average_change)
+
+
+
+    
+
+    
+    # with open('budget_data.csv','w') as csv_budget_two:
+    #     csv_dictreader = csv.DictReader(csv_budget_two)
+        
+        
+    #     fieldnames=['Date','Profit/Losses']
+
+    #     csv_writer = csv.DictWriter(csv_budget_two,fieldnames = fieldnames, delimeters = ',t')
+                
+    #     csv_writer.writeheader()
+
+    
+    
